9234eea262c1c1bfaa4264709f4d3a36fc79ea97
Jan Izyk <jan.izyk@dbaudio.com>
2020-04-30 20:24:46 +0000

    wip

--- unitTests/main.cpp
+++ unitTests/main.cpp
@@ -5,22 +5,34 @@
 
 #include <tuple>
 #include <memory>
+#include <string>
 #include <gtest/gtest.h>
 
 #include "TxtController/Controller.hpp"
 
 namespace {
-class ControllerParameterizedMethodPrepareMotor1
-    : public ::testing::TestWithParam<std::tuple<uint16_t, int16_t, int16_t>> {
+class PrepareMotor1
+    : public ::testing::TestWithParam<std::tuple<std::string, uint16_t, int16_t, int16_t>> {
+public:
+    struct PrintToStringParamName {
+        template <class ParamType>
+        std::string operator()(const testing::TestParamInfo<ParamType>& info) const
+        {
+            auto testData =
+                static_cast<std::tuple<std::string, uint16_t, int16_t, int16_t>>(info.param);
+            return std::get<0>(testData);
+        }
+    };
+
 protected:
     std::shared_ptr<jatozy::TxtController::Controller> controller;
 };
 
-TEST_P(ControllerParameterizedMethodPrepareMotor1, PrepareMotor1)
+TEST_P(PrepareMotor1, DataIsSet)
 {
-    const uint16_t distance = std::get<0>(GetParam());
-    const int16_t rotationSpeed1 = std::get<1>(GetParam());
-    const int16_t rotationSpeed2 = std::get<2>(GetParam());
+    const uint16_t distance = std::get<1>(GetParam());
+    const int16_t rotationSpeed1 = std::get<2>(GetParam());
+    const int16_t rotationSpeed2 = std::get<3>(GetParam());
 
     FISH_X1_TRANSFER hardwareInterface;
     controller = std::shared_ptr<jatozy::TxtController::Controller>(
@@ -34,16 +46,15 @@ TEST_P(ControllerParameterizedMethodPrepareMotor1, PrepareMotor1)
     EXPECT_EQ(rotationSpeed2, hardwareInterface.ftX1out.duty[1]);
 }
 
-INSTANTIATE_TEST_CASE_P(ControllerTests,
-                        ControllerParameterizedMethodPrepareMotor1,
-                        ::testing::Values(std::make_tuple(127, 512, 0),
-                                          std::make_tuple(127, 0, 512),
-                                          std::make_tuple(64, 512, 0),
-                                          std::make_tuple(64, 0, 512),
-                                          std::make_tuple(127, 256, 0),
-                                          std::make_tuple(127, 0, 256),
-                                          std::make_tuple(64, 256, 0),
-                                          std::make_tuple(64, 0, 256)));
+INSTANTIATE_TEST_CASE_P(Controller,
+                        PrepareMotor1,
+                        ::testing::Values(
+                            std::make_tuple("OneRotation_CounterClockwise_FullSpeed", 127, 512, 0),
+                            std::make_tuple("OneRotation_Clockwise_FullSpeed", 127, 0, 512),
+                            std::make_tuple("HalfRotation_CounterClockwise_FullSpeed", 64, 512, 0),
+                            std::make_tuple("OneRotation_CounterClockwise_QuarterSpeed", 127, 128, 0),
+                            std::make_tuple("RotateForteenDegree_Clockwise_TenPercentSpeed", 5, 0, 51)),
+                        PrepareMotor1::PrintToStringParamName());
 
 } // namespace
 
