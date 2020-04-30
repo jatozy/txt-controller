745b4937c0cc82efd9555229d6e4797dd7d75ec4
Jan Izyk <jan.izyk@dbaudio.com>
2020-04-30 23:10:19 +0000

    Add integration tests

---
+++ integrationTests/main.cpp
@@ -0,0 +1,20 @@
+// clang-format off
+#include <KeLibTxtDl.h>
+#include <FtShmem.h>
+// clang-format on
+
+#include <tuple>
+#include <memory>
+#include <string>
+#include <gtest/gtest.h>
+
+#include "TxtController/Controller.hpp"
+
+namespace {
+} // namespace
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
